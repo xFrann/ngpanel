52bf19fb17487330adc44228b53e20731ec5da03
Frangu Madalin <polarfrann@gmail.com>
2022-02-14 00:04:05 +0200

    adding sign in page

--- src/components/material/SignInSide.js
+++ src/components/material/SignInSide.js
@@ -10,7 +10,7 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider, ThemeOptions } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
