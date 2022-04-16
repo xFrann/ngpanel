c7fe3a8f03074be58ca04cb48cfa7de1d2d511be
Frangu Madalin <polarfrann@gmail.com>
2022-04-16 14:03:02 +0300

    Adjusted Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import SignInSide from './containers/SignInSide'
 import Dashboard from './containers/Dashboard'
+import Announcements from './containers/Announcements'
+import Statistics from './containers/Statistics'
+import Website from './containers/Website'
+import Content from './containers/Content'
+import Register from './containers/Register'
 const App = () => {
 
   return (
@@ -9,7 +14,13 @@ const App = () => {
         <Routes>
           {/* <Route path="/" element={SignInSide}></Route> */}
           <Route path="/" element={<SignInSide></SignInSide>}></Route>
-          <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+          <Route path='/register' element={<Register></Register>}></Route>
+          <Route path="/dashboard/*" element={<Dashboard></Dashboard>}>
+            <Route path="annoucements" element={<Announcements></Announcements>}></Route>  
+            <Route path="statistics" element={<Statistics></Statistics>}></Route>
+            <Route path="website" element={<Website></Website>}></Route>
+            <Route path="content" element={<Content></Content>}></Route>
+          </Route>
         </Routes>
       </Router>
   )
