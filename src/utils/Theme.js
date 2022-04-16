dfec1b6f096eb10d32f230898e4cf4f50a5f2d3b
Frangu Madalin <polarfrann@gmail.com>
2022-04-16 14:07:51 +0300

    Testing different styles

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -12,4 +12,14 @@ export const themeOptions = {
       },
       divider: 'rgba(255,255,255,0.67)',
     },
+    components: {
+      MuiDrawer: {
+        styleOverrides: {
+          paper: {
+            background: 'linear-gradient(45deg, rgba(0,128,128,1) 0%, rgba(95,255,148,1) 100%);',
+
+          }
+        }
+      },
+    },
   };
