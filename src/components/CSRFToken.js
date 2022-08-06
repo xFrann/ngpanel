2ef35e16071d2a72627433b8d1f30888cf111297
Frangu Madalin <polarfrann@gmail.com>
2022-08-07 01:59:41 +0300

    small changes

--- src/components/CSRFToken.js
+++ src/components/CSRFToken.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react'; 
-import { ContactlessOutlined } from '@mui/icons-material';
+
+
+const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 /**
  * 
  * @returns hidden input field that send CSRF token with form HTTP Requests
@@ -10,20 +26,6 @@ export default function CSRFToken() {
 
     const [csrftoken, setcsrftoken] = useState('');
 
-    const getCookie = (name) => {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            let cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++) {
-                let cookie = cookies[i].trim();
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -41,4 +43,8 @@ export default function CSRFToken() {
   return (
     <input type='hidden' name='csrfmiddlewaretoken' value={"test"}/>
   )
+}
+
+export const isCSRFTokenSet = () => {
+    return getCookie('csrftoken') ? true : false;
 }
