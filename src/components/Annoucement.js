db0b311a846be712d8c8af869dd5509ea5a4508b
Frangu Madalin <polarfrann@gmail.com>
2022-08-07 01:58:36 +0300

    display comments, destructure props

--- src/components/Annoucement.js
+++ src/components/Annoucement.js
@@ -2,19 +2,13 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import UserPreview from './UserPreview';
+import NotesIcon from '@mui/icons-material/Notes';
 import ProfileAvatar from './ProfileAvatar';
 import { useFetchedData } from '../customHooks/useFetchData';
 import Comment from './Comment';
@@ -32,25 +26,27 @@ const ExpandMore = styled((props) => {
 
 export default function Annoucement(props) {
     const [expanded, setExpanded] = React.useState(false);
-
+    const { announcement } = props;
+    const { id, title, date, content, user } = announcement;
     const handleExpandClick = () => {
       setExpanded(!expanded);
     };
 
-    const {fetchedData: comment, loading} = useFetchedData(`http://127.0.0.1:8000/announcements/${props.id}/comments/`)
+    const {fetchedData: comment, loading} = useFetchedData(`http://127.0.0.1:8000/announcements/${id}/comments/`)
+    
 
   return (
     <Card sx={{ maxWidth: 500 }}>
       <CardHeader
         avatar={
-            <ProfileAvatar></ProfileAvatar>
+            <ProfileAvatar username={user}></ProfileAvatar>
         }
-        title={props.title}
-        subheader={props.date}
+        title={title}
+        subheader={date}
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-            {props.content}
+            {content}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -63,13 +59,13 @@ export default function Annoucement(props) {
           aria-expanded={expanded}
           aria-label="show more"
         >
-          <ExpandMoreIcon />
+          <NotesIcon />
         </ExpandMore>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <Collapse in={expanded} timeout="auto" >
         <CardContent>
-          <Typography paragraph>Comentarii</Typography>
-          {loading ? <p>"loading..."</p> : comment.map((object, i) => <Comment user={props.user} date={comment[i].date} comment={comment[i].content}></Comment>)}
+          {loading ? <p>"loading..."</p> :
+           comment.map((object, i) => <Comment key={`comment_${i}`} comment={comment[i]}></Comment>)}
         </CardContent>  
       </Collapse>
     </Card>
