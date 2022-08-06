43a56f2bb585647b3ef90dff1b1e96359195210f
Frangu Madalin <polarfrann@gmail.com>
2022-08-07 02:03:59 +0300

    empty comp

--- src/containers/Statistics.js
+++ src/containers/Statistics.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Statistics() {
   return (
-    <div>Statistics</div>
+    <>
+    <div>
+      <p>Statisticle 100% smecher</p>
+    </div>
+    </>
   )
 }
