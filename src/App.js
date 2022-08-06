64a5e247fbbb9354dfd73dbe2862ba4403732bb2
Frangu Madalin <polarfrann@gmail.com>
2022-08-07 01:56:03 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Statistics from './containers/Statistics'
 import Website from './containers/Website'
 import Content from './containers/Content'
 import Register from './containers/Register'
+import RegisterProfile from './containers/RegisterProfile'
+import RegisterDetails from './containers/RegisterDetails'
+import "./index.css"
+import Profile from './containers/Profile'
 const App = () => {
 
   return (
@@ -15,12 +19,16 @@ const App = () => {
           {/* <Route path="/" element={SignInSide}></Route> */}
           <Route path="/" element={<SignInSide></SignInSide>}></Route>
           <Route path='/register' element={<Register></Register>}></Route>
+          <Route path='/registerProfile' element={<RegisterProfile></RegisterProfile>}></Route>
+          <Route path='/registerDetails' element={<RegisterDetails></RegisterDetails>}></Route>
           <Route path="/dashboard/*" element={<Dashboard></Dashboard>}>
             <Route path="annoucements" element={<Announcements></Announcements>}></Route>  
             <Route path="statistics" element={<Statistics></Statistics>}></Route>
             <Route path="website" element={<Website></Website>}></Route>
             <Route path="content" element={<Content></Content>}></Route>
+            
           </Route>
+          <Route path='profile/:username' element={<Profile></Profile>}></Route>
         </Routes>
       </Router>
   )
