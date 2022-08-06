bb8cb2acaee325dc532b72ced9502bee27efb7dd
Frangu Madalin <polarfrann@gmail.com>
2022-08-07 02:02:37 +0300

    visual cues on form

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,8 +1,35 @@
-import { Grid, TextField } from '@mui/material'
-import React from 'react'
+import { Alert, Grid, TextField } from '@mui/material'
+import React, { useEffect } from 'react'
+import { Navigate } from 'react-router-dom'
+import { validateCode } from '../actions/register'
+import Notification from '../components/notifications/Notification'
+
 
 export default function Register() {
+
+  const [code, setCode] = React.useState('')
+  const [valid, setValid] = React.useState(false)
+  const [full, setFull] = React.useState(false)
+  const [redirect, setRedirect] = React.useState(false)
+  
+  useEffect(() => { 
+    if (full) {
+      validateCode(code).then(res => {
+        if (res.success) {
+          setValid(true)
+          localStorage.setItem('code', code)
+          setTimeout(() => setRedirect(true), 1000);
+        } else {
+          setValid(false)
+        }
+      })
+    }
+
+   }, [code, validateCode, valid])
+
+
   return (
+
     <Grid
     container
     spacing={0}
@@ -20,11 +47,17 @@ export default function Register() {
                 id="code"
                 label="Invite Code"
                 name="code"
-                // onChange={e => onChange(e)}
                 autoFocus
+                error={!valid && full}
+                color={!valid ? null : full ? 'success' : null }
+                onChange={e => {
+                  setCode(e.target.value)
+                  setFull(e.target.value.length == 6)
+                }}
               />
+          {valid ? <Notification severity='success'>Code is valid!</Notification> : full ? <Notification severity='error'>Invalid Code!</Notification> : null }
+          {redirect ? <Navigate to="/registerProfile"></Navigate> : null}
         </Grid>   
-   
     </Grid> 
   )
 }
