94c5f1e13440a1374db39645437212d1968056da
Frangu Madalin <polarfrann@gmail.com>
2022-08-07 02:00:36 +0300

    testing only

--- src/components/UserPreview.js
+++ src/components/UserPreview.js
@@ -1,23 +1,106 @@
-import { Avatar, Chip } from '@mui/material'
-import React from 'react'
-
+import { Avatar, Chip } from '@mui/material';
+import React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import { Box } from '@mui/system';
+import { logout } from '../actions/auth'
+import { useNavigate } from 'react-router-dom';
+import ProfileAvatar from './ProfileAvatar';
 export default function UserPreview(props) {
 
+  const navigate = useNavigate()
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <Chip
-    avatar={
-    <Avatar 
-    alt={props.user.username.toUpperCase()} 
-    src="http://127.0.0.1:8000/static/profile.jpg"
-    onClick={() => console.log("avatar click")}
-    />
-    }
-    onClick={() => console.log("User clicked")}
-    label={props.user.first_name + " " + props.user.last_name}
-    variant="outlined"
-    sx={props.sx}
-  />
+  <Box sx={props.sx}>
+  <Tooltip title="Account settings" sx={props.sx}>
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <ProfileAvatar username={props.user.username}  sx={{ width: 32, height: 32 }}></ProfileAvatar>
+          </IconButton>
+        </Tooltip>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem>
+          <Avatar /> Profile
+        </MenuItem>
+        <MenuItem>
+          <Avatar /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={() => logout().then(res => res ? navigate("/") : alert("error while logging out"))}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+      </Box>
   )
 }
